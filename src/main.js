17052449d3ccbf3e54257ac8f405dbfd600fd3ce
John Becker <jbec1901@gmail.com>
2018-04-18 08:55:08 -0500

    moving off of sudo code

--- src/main.js
+++ src/main.js
@@ -40,3 +40,80 @@ draw() {
 	run all of your drawing here
 }
 */
+
+
+const {app, BrowserWindow} = require('electron');
+
+const tickPerSecond = 30;
+
+GameMode = 1;
+gameRunning = true;
+
+let mainWindow;
+
+app.on('ready', () => {
+	mainWindow = new BrowserWindow({
+		height: 600,
+		width: 800
+	});
+
+	mainWindow.loadURL('file://' + __dirname + '/canvas.html');
+	
+	startup()
+	lastTime = getTime();
+	while (gameRunning) {
+		switch (GameMode) {
+		case 0:
+			//menu
+			break;
+		case 1:
+			var newTime = getTime();
+			var delta += newTime - lastTime;
+			lastTime = newTime;
+			if (delta > 1 / TicksPerMillisecond) {
+				delta = 0;
+				tick();
+			}
+			keyPressFunctions();
+			DrawScreen();
+			break;
+		default:
+			gameRunning = false;
+			break;
+		}
+	}
+});
+
+function getTime(){
+	return new Date().getTime();
+}
+
+function startup(){
+	camera = new Camera();
+	
+	entitys = [];
+	
+	addKeyListener();
+	
+	var anchor = document.getElementById('canvasAnchor');
+	
+	canvas = document.createElement('canvas');
+	document.body.insertBefore(canvas, anchor);
+	
+	ctx = canvas.getContext('2d');
+	
+	function resizeCanvas() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+  	}
+	resizeCanvas();
+	window.addEventListener('resize', resizeCanvas, false);
+}
+
+function tick(){
+	
+}
+
+function draw(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
