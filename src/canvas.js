b3980a5e432ab4af787a6b861cd73fd654cda7e9
Steven Kaufman <steven.kaufman0813@gmail.com>
2018-04-20 10:56:06 -0500

    Update canvas.js

--- src/canvas.js
+++ src/canvas.js
@@ -24,19 +24,3 @@ var Canvas = {
         return this.canvas;
     }
 };
-
-var Render = {
-    framerate : 30,
-    flag : false,
-    start : function(){
-        this.run();
-    },
-    stop : function(){
-        this.flag = true;
-    },
-    
-    run : function(){
-        if(window.render) window.render();
-        if(!Render.flag) setTimeout(Render.run, 1000 / Render.framerate);
-    }
-};
