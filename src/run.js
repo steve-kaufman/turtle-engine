c265d2ed07ebec18e91a51d8dd66153f1dadd4a5
Steven Kaufman <steven.kaufman0813@gmail.com>
2018-04-20 10:56:22 -0500

    Update run.js

--- src/run.js
+++ src/run.js
@@ -1,1 +1,15 @@
-
+var Render = {
+    framerate : 30,
+    flag : false,
+    start : function(){
+        this.run();
+    },
+    stop : function(){
+        this.flag = true;
+    },
+    
+    run : function(){
+        if(window.render) window.render();
+        if(!Render.flag) setTimeout(Render.run, 1000 / Render.framerate);
+    }
+};
