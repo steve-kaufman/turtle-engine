22043723c6c25ea3e7459ed7e57f4e91602e85ba
Steven Kaufman <steven.kaufman0813@gmail.com>
2018-04-20 10:54:50 -0500

    Update entity.js

--- src/entity.js
+++ src/entity.js
@@ -1,29 +1,32 @@
-function Entity(x,y,width,height){
+function Entity(x, y, width, height, options){
     
-	// CollisonBox = ?;
-	
 	this.x = x || 0;
 	this.y = y || 0;
-	
-	this.velocityX = 0;
-	this.velocityY = 0;
-	
 	this.width = width || 32;
-	this.height = height || 64;
+	this.height = height || 32;
+	options = options || {};
 	
-	// this.sprite = ?;
+	var centerX = this.x + this.width / 2;
+	var centerY = this.y + this.height / 2;
+	this.physical = Matter.Bodies.rectangle(centerX, centerY, 
+											this.width, this.height, 
+											options);
+	Matter.World.add(engine.world, this.physical);
 	
-	this.draw = function(ctx){
-		
-	}
+	this.sprite = null;
+	this.canvas = Canvas.canvas;
+	this.render = function(sprite, canvas){
+		this.sprite.drawAngled(
+			this.physical.angle, this.x, this.y, 
+			this.width, this.height, this.canvas
+			);
+	};
 	
-	this.run = function(){
-		var newX = this.x + this.velocityX;
-		var newY = this.y + this.velocityY;
-		
-		//Test collations here on the new point and only move to them if the space is open
-		
-		this.x = newX;
-		this.y = newY;
-	}
+	this.rotates = false;
+	this.update = function(){
+		if(!this.rotates) Matter.Body.setAngle(this.physical, 0);
+		this.x = this.physical.x - this.width / 2;
+		this.y = this.physical.y - this.height / 2;
+	};
+	
 }
