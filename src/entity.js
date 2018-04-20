238667bafd7497477646053746545c456dcc9c29
John Becker <jbec1901@gmail.com>
2018-04-20 09:45:42 -0500

    Separating Ticks and drawing

--- src/entity.js
+++ src/entity.js
@@ -1,3 +1,29 @@
-function Entity(){
+function Entity(x,y,width,height){
     
+	// CollisonBox = ?;
+	
+	this.x = x || 0;
+	this.y = y || 0;
+	
+	this.velocityX = 0;
+	this.velocityY = 0;
+	
+	this.width = width || 32;
+	this.height = height || 64;
+	
+	// this.sprite = ?;
+	
+	this.draw = function(ctx){
+		
+	}
+	
+	this.run = function(){
+		var newX = this.x + this.velocityX;
+		var newY = this.y + this.velocityY;
+		
+		//Test collations here on the new point and only move to them if the space is open
+		
+		this.x = newX;
+		this.y = newY;
+	}
 }
