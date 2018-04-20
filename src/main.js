bc43fb905f0ccb8b603e5b1d73c6e62b0154f9f9
Steven Kaufman <steven.kaufman0813@gmail.com>
2018-04-20 10:59:58 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,11 +7,7 @@ function init(){
     Canvas.create();
         
     Render.start();
-	
-	running = true;
-	
-	//Run a tick based on tps
-	mainLoop = setInterval(tick,tps * 1000);
+	Update.start();
 }
 
 function render(){
@@ -19,16 +15,10 @@ function render(){
     //Do all rendering below here
 }
 
-function tick(){
-	//If game has stopped stop the ticks
-	if(!running) {
-		clearInterval(mainLoop);
-		Render.stop();
-	}
-	
+function update(){
 	//Run what needs to be calculated for each entity on each tick
 	for(entity in entities){
-		entity.run();
+		entity.update();
 	}
 }
 
