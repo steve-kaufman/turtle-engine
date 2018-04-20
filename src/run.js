905016ba245742b000fd1709cadac24b85891084
Steven Kaufman <steven.kaufman0813@gmail.com>
2018-04-20 11:07:03 -0500

    Update run.js

--- src/run.js
+++ src/run.js
@@ -1,15 +1,31 @@
 var Render = {
     framerate : 30,
-    flag : false,
+    running : false,
     start : function(){
         this.run();
     },
     stop : function(){
-        this.flag = true;
+        this.running = false;
     },
     
     run : function(){
         if(window.render) window.render();
-        if(!Render.flag) setTimeout(Render.run, 1000 / Render.framerate);
+        if(Render.running) setTimeout(Render.run, 1000 / Render.framerate);
+    }
+};
+
+var Update = {
+    framerate : 30,
+    running : true,
+    start : function(){
+        this.run();
+    },
+    stop : function(){
+        this.running = false;
+    },
+    
+    run : function(){
+        if(window.update) window.update();
+        if(Update.running) setTimeout(Update.run, 1000 / Update.framerate);
     }
 };
